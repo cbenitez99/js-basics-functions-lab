2cd4e25a9f6933930717ea667a53c4411b113552
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-05-23 01:48:10 -0500

    commit tree 126e8d63231dc3708b1028845b46886db031dd4d

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+  if (destination > 42) {
+    return destination - 42;
+  } else {
+    return 42 - destination;
+  }
+}
+function distanceFromHqInFeet(destination) {
+  return distanceFromHqInBlocks(destination) * 264;
+}
+
+function distanceTravelledInFeet(beginning, destination) {
+  if (beginning < destination) {
+    return (destination - beginning) * 264;
+  } else {
+    return (beginning - destination) * 264;
+  }
+}
+
+function calculatesFarePrice(beginning, destination) {
+  const distance = distanceTravelledInFeet(beginning, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return 0.02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+//takes start location and returns the number 
+//of blocks its driven from hq on 42nd. 
+//(starts on 42)
+
+
+
+
